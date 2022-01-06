16167a46d18c2c8ca68ae204375a0bb25f0a21e1
Anand Jee Choubey <anandjeechoubey@Anands-MacBook-Air.local>
2022-01-07 00:56:20 +0530

    Init Middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,15 +2,18 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export default async (req) => {
-    const token = getToken({ req, secret: process.env.JWT_SECRET });
+    const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
-    const { pathname } = req.nextUrl;
+    // const { pathname } = req.nextUrl;
 
-    if (pathname.includes("/api/auth/") || token) {
-        return NextResponse.next();
-    }
+    // if (pathname.includes("/api/auth/") || token) {
+    //     return NextResponse.next();
+    // }
 
-    if (!token && pathname !== '/login') {
-        return NextResponse.redirect('/login');
-    }
+    // if (!token && pathname !== '/login') {
+    //     return NextResponse.redirect('/login');
+    // }
+
+    // by passing middleware
+    return NextResponse.redirect('/login');
 };
